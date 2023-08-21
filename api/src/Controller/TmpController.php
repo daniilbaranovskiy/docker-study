c682135f6ba0d74268aff814e1ae037491d9687a
Daniil Baranovskiy <daniilbaranovskiy03@gmail.com>
2023-08-21 21:14:57 +0300

    docker-study-symfony v4

--- api/src/Controller/TmpController.php
+++ api/src/Controller/TmpController.php
@@ -3,26 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class TmpController extends AbstractController
 {
+    /**
+     * @var UserPasswordHasherInterface
+     */
+    private UserPasswordHasherInterface $passwordHasher;
     /**
      * @var EntityManagerInterface
      */
     private EntityManagerInterface $entityManager;
 
     /**
+     * @param UserPasswordHasherInterface $passwordHasher
      * @param EntityManagerInterface $entityManager
      */
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager)
     {
 
+        $this->passwordHasher = $passwordHasher;
         $this->entityManager = $entityManager;
     }
 
@@ -34,17 +43,17 @@ class TmpController extends AbstractController
     #[Route(path: "tmp", name: "app_tmp")]
     public function tmp(Request $request): JsonResponse
     {
-        $requestData = $request->query->all();
-        $products = $this->entityManager->getRepository(Product::class)->getAllProductsByName(
-            $requestData['itemsPerPage'] ?? 30,
-            $requestData['page'] ?? 1,
-            $requestData['name'] ?? null,
-            $requestData['categoryName'] ?? null,
-
-
+        $pass = "test1234";
+        $user = new User();
+        $user->setEmail("test@gmail.com");
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            $pass
         );
+        $user->setPassword($hashedPassword);
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
 
-
-        return new JsonResponse($products);
+        return new JsonResponse();
     }
 }
---
+++ api/src/Entity/User.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    public const  ROLE_USER = "ROLE_USER";
+
+    /**
+     * @var int|null
+     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    /**
+     * @var string|null
+     */
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
+
+    /**
+     * @var array
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    public function __construct()
+    {
+        $this->roles = [self::ROLE_USER];
+    }
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return $this
+     */
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string)$this->email;
+    }
+
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
+    {
+        return (string)$this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param array $roles
+     * @return $this
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return $this
+     */
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+}
